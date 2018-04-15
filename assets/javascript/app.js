4c3a034094fafd126c516c2695b02ae811013396
Austin Wagner <austin.e.wagner@gmail.com>
2018-04-14 18:11:25 -0700

    able to join game as guest

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -29,8 +29,28 @@ $(document).ready(function () {
     let turn = initialTurn;
     let choice = "";
     let guestCount = 0;
+    let messageCount = 0;
+    let myUsername = "";
+    let guestId = "";
+    // let disconnectedUser = "";
 
-//TRACKING NUMBER OF PLAYERS //////////////////////////////////////////////////////////////
+
+    // $('#chat-box').hide();
+
+    //TRACKING EVERYTHING ////////////////////////////////////////////////////////////////
+    database.ref('/').on('value', snap => {
+        // console.log(`***GLOBAL SNAP****`, snap.val());
+
+        if (snap.child('guests').exists() && snap.child('chat').exists() && snap.numChildren() === 2 && snap.val().guests.count.count === 0) {
+            database.ref('/chat').remove();
+            $('.chat-message').empty();
+        }
+
+    });
+
+
+
+    //TRACKING NUMBER OF PLAYERS /////////////////////////////////////////////////////////
     playersRef.on('value', (snap) => {
 
         if (snap.numChildren() === 1) {
@@ -42,6 +62,11 @@ $(document).ready(function () {
                 $('.player-two-empty').empty();
                 // $('#player-two-score').empty();
                 $('#player-two-username').text(`Waiting for Player 2`);
+
+                if (myUsername && youAre !== 'playerOne') {
+                    $('#player-two-rock').append(`<button type="button" class="btn btn-success join-game-button">Join Game!</button>`);
+                }
+
             }
 
             else {
@@ -50,28 +75,43 @@ $(document).ready(function () {
                 $('.player-one-empty').empty();
                 // $('#player-one-score').empty();
                 $('#player-one-username').text(`Waiting for Player 1`);
+
+                if (myUsername && youAre !== 'playerTwo') {
+                    $('#player-one-rock').append(`<button type="button" class="btn btn-success join-game-button">Join Game!</button>`);
+                }
             }
 
 
         }
 
-        console.log('SOMETHING HAPPENED');
+        else if (snap.numChildren() === 0) {
+            console.log('All Gone')
+            $('.rps-choice').empty();
+            $('#result-card').empty();
+            $('.player-two-empty').empty();
+            $('.rps-choice').empty();
+            $('#result-card').empty();
+            $('.player-one-empty').empty();
+        }
+
+        // console.log('SOMETHING HAPPENED');
 
     });
 
 
-// CLICK EVENT FOR ADDING NEW PLAYERS /////////////////////////////////////////////////////
+    // CLICK EVENT FOR ADDING NEW PLAYERS ////////////////////////////////////////////////
 
     $('#username-button').on('click', function () {
         event.preventDefault();
 
         let player = $('#username-input').val().trim();
 
+        myUsername = player;
 
         playersRef.once('value').then(function (snapshot) {
             numPlayers = snapshot.numChildren();
 
-            console.log('numPlayers 2', numPlayers);
+            // console.log('numPlayers 2', numPlayers);
 
             if (numPlayers === 0) {
 
@@ -83,13 +123,13 @@ $(document).ready(function () {
 
                 database.ref('/players/p1').set({
                     username: player,
-                    wins: p1Wins,
-                    losses: p1Losses
+                    wins: 0,
+                    losses: 0
                 });
 
 
 
-                console.log(`You are: ${youAre}`);
+                // console.log(`You are: ${youAre}`);
             }
 
             if (numPlayers === 1 && snapshot.child('p1').exists()) {
@@ -102,8 +142,8 @@ $(document).ready(function () {
 
                 database.ref('/players/p2').set({
                     username: player,
-                    wins: p2Wins,
-                    losses: p2Losses
+                    wins: 0,
+                    losses: 0
                 });
 
                 database.ref('/turns').set({
@@ -112,7 +152,7 @@ $(document).ready(function () {
 
 
 
-                console.log(`You are: ${youAre}`);
+                // console.log(`You are: ${youAre}`);
             }
 
             if (numPlayers === 1 && snapshot.child('p2').exists()) {
@@ -124,8 +164,8 @@ $(document).ready(function () {
 
                 database.ref('/players/p1').set({
                     username: player,
-                    wins: p1Wins,
-                    losses: p1Losses
+                    wins: 0,
+                    losses: 0
                 });
 
                 database.ref('/turns').set({
@@ -134,17 +174,18 @@ $(document).ready(function () {
 
 
 
-                console.log(`You are: ${youAre}`);
+                // console.log(`You are: ${youAre}`);
             }
 
             if (numPlayers === 2) {
                 guestCount++;
-                console.log(guestCount);
+                // console.log(guestCount);
 
                 $('#player-name').text(`Hi ${player}! the game is full, but you can watch and chat!`);
 
                 youAre = `guest${guestCount}`;
-                console.log('TEST You are:', youAre)
+                guestId = `guest${guestCount}`;
+                // console.log('TEST You are:', youAre)
 
 
                 database.ref(`/guests/${youAre}`).set({
@@ -161,13 +202,48 @@ $(document).ready(function () {
             connectionTracker(youAre);
 
 
+
+            $('#chat-box').html(`<div class="row justify-content-center">
+            <div class="col-xs-12 col-md-10">
+                <div class="card" id="chat-card">
+                    <div class="card-body" id="chat-card-body">
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-xs-12 col-md-10">
+                <form>
+                    <div class="input-group mb-3">
+                        <input type="text" class="form-control" placeholder="Message" aria-label="Message" aria-describedby="basic-addon2" id="chat-message">
+                        <div class="input-group-append">
+                            <button class="btn btn-outline-secondary" type="submit" id="chat-button">Send</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>`);
+
+
+            database.ref('/chat').push({
+                message: `${myUsername} has joined the chat`,
+                sender: 'Chatbot'
+            })
+
         });
     });
 
-// TRACKING PLAYER ONE ////////////////////////////////////////////////////////////////////
+    // CLICK EVENT TO JOIN GAME ///////////////////////////////////////////////////////////
+
+    $(document.body).on('click', '.join-game-button', function () {
+        joinGame();
+    });
+
+    // TRACKING PLAYER ONE ///////////////////////////////////////////////////////////////
 
     database.ref('/players/p1').on('value', function (snapshot) {
-        console.log('Player One Snap', snapshot.val());
+        // console.log('Player One Snap', snapshot.val());
         if (snapshot.val()) {
             playerOne = snapshot.val().username;
             p1Wins = snapshot.val().wins;
@@ -177,17 +253,17 @@ $(document).ready(function () {
             $('#player-one-score').html(`Wins: ${p1Wins} Losses: ${p1Losses}`);
 
             if (snapshot.child('choice').exists()) {
-                console.log('fb choice: ', snapshot.val().choice);
+                // console.log('fb choice: ', snapshot.val().choice);
                 p1Choice = snapshot.val().choice
             }
 
         }
     });
 
-// TRACKING PLAYER TWO/////////////////////////////////////////////////////////////////////
+    // TRACKING PLAYER TWO////////////////////////////////////////////////////////////////
 
     database.ref('/players/p2').on('value', function (snapshot) {
-        console.log('Player Two Snap', snapshot.val());
+        // console.log('Player Two Snap', snapshot.val());
         if (snapshot.val()) {
             playerTwo = snapshot.val().username;
             p2Wins = snapshot.val().wins;
@@ -199,42 +275,45 @@ $(document).ready(function () {
             $('#player-two-score').html(`Wins: ${p2Wins} Losses: ${p2Losses}`);
 
             if (snapshot.child('choice').exists()) {
-                console.log('fb choice: ', snapshot.val().choice);
+                // console.log('fb choice: ', snapshot.val().choice);
                 p2Choice = snapshot.val().choice
 
-                console.log(`${playerOne} chose ${p1Choice} and ${playerTwo} chose ${p2Choice}`);
+                // console.log(`${playerOne} chose ${p1Choice} and ${playerTwo} chose ${p2Choice}`);
 
             }
         }
     });
 
-// TRACKING NUMBER OF TURNS ///////////////////////////////////////////////////////////////
+    // TRACKING NUMBER OF TURNS //////////////////////////////////////////////////////////
 
     database.ref('/turns').on('value', function (snapshot) {
-        console.log('NEW TURN');
+        // console.log('NEW TURN');
 
         if (snapshot.child("turn").exists()) {
 
-            console.log(snapshot.val());
+            // console.log(snapshot.val());
 
             turn = snapshot.val().turn;
 
-            console.log('Turn updated:', turn);
+            // console.log('Turn updated:', turn);
 
-            console.log(`You are: ${youAre}`);
+            // console.log(`You are: ${youAre}`);
 
             turn = snapshot.val().turn;
 
             $('.rps-choice').empty();
 
             if (turn === 3) {
+                if (youAre === 'playerTwo') {
+                    $('#player-two-paper').html(`<h2>${p2Choice}</h2>`);
+                }
                 rpsLogic(p1Choice, p2Choice);
             }
 
             else if (turn === 2) {
 
                 if (youAre === 'playerTwo') {
-                    console.log('Player Two its your turn!');
+                    // console.log('Player Two its your turn!');
 
                     $('#player-turn').text(`It's your turn!`);
 
@@ -247,6 +326,7 @@ $(document).ready(function () {
 
                 if (youAre === 'playerOne') {
                     $('#player-turn').text(`Waiting for ${playerTwo} to choose`);
+                    $('#player-one-paper').html(`<h2>${p1Choice}</h2>`);
                 }
 
             }
@@ -254,7 +334,7 @@ $(document).ready(function () {
             else if (turn === 1) {
 
                 if (youAre === 'playerOne') {
-                    console.log('Player One its your turn!');
+                    // console.log('Player One its your turn!');
 
                     $('#player-turn').text(`It's your turn!`);
 
@@ -275,7 +355,7 @@ $(document).ready(function () {
 
     });
 
-    // CLICK EVENT THAT INITIATES GAME LOGIC //////////////////////////////////////////////
+    // CLICK EVENT THAT INITIATES GAME LOGIC /////////////////////////////////////////
 
     $(document.body).on('click', '.rps-selection', function () {
 
@@ -283,7 +363,7 @@ $(document).ready(function () {
 
         if (youAre === 'playerOne') {
             p1Choice = $(this).attr('data-value');
-            console.log(p1Choice);
+            // console.log(p1Choice);
             database.ref('/players/p1').update({
                 choice: p1Choice
             });
@@ -295,7 +375,7 @@ $(document).ready(function () {
 
         if (youAre === 'playerTwo') {
             p2Choice = $(this).attr('data-value');
-            console.log(p2Choice);
+            // console.log(p2Choice);
             database.ref('/players/p2').update({
                 choice: p2Choice
             });
@@ -305,19 +385,67 @@ $(document).ready(function () {
             });
         }
 
+        
+
     });
 
-// TRACKING WHEN A PLAYER DISCONNECTS /////////////////////////////////////////////////////
+    // CHAT LOGIC ////////////////////////////////////////////////////////////////////////
+
+    $(document.body).on('click', '#chat-button', function () {
+        event.preventDefault();
+        let message = $('#chat-message').val().trim();
+
+        database.ref('/chat').push({
+            message: message,
+            sender: myUsername
+        })
+
+    });
+
+
+    database.ref('/chat').on('child_added', snap => {
+        let newMessage = snap.val().message;
+        let newSender = snap.val().sender;
+
+        $('#chat-card-body').append(`<p class="chat-message from-${newSender}">${newSender}:  ${newMessage}</p>`);
+
+        $(`.from-${myUsername}`).css('color', 'blue');
+        $('.from-Chatbot').css('width', '100%');
+        $('.from-Chatbot').css('background-color', 'rgb(199, 199, 199)');
+
+    });
+
+
+    // TRACKING WHEN A PLAYER DISCONNECTS ////////////////////////////////////////////////
 
     playersRef.on('child_removed', function (snapshot) {
-        console.log(snapshot.val());
+        // console.log(snapshot.val());
 
-        console.log(`${snapshot.val().username} left the game :(`);
+        // console.log(`${snapshot.val().username} left the game :(`);
+
+        let disconnectedUser = snapshot.val().username;
 
         database.ref('/turns').remove();
+
+        database.ref(`/chat/${disconnectedUser}`).set({
+            message: `${disconnectedUser} has disconnected`,
+            sender: 'Chatbot'
+        });
+
     });
 
-// TRACKING THE GUEST COUNT ///////////////////////////////////////////////////////////////
+    // playersRef.on('child_added', snap => {
+    //     database.ref('/chat').once('value', snap => {
+    //         if (snap.child(myUsername).exists()){
+    //             database.ref(`/chat/${myUsername}`).set({
+    //                 message: `${disconnectedUser}" has joined the game`,
+    //                 sender: 'Chatbot'
+    //             });
+    //         }
+    //     });
+    // });
+
+    // TRACKING THE GUEST COUNT //////////////////////////////////////////////////////////
 
 
     database.ref('/guests/count').on('value', (snap) => {
@@ -334,16 +462,24 @@ $(document).ready(function () {
         }
     });
 
-// TRACKING WHEN A GUEST LEAVES ///////////////////////////////////////////////////////////
+    // TRACKING WHEN A GUEST LEAVES //////////////////////////////////////////////////////
 
-    guestRef.on('child_removed', function (snapshot) {
+    guestRef.once('child_removed', function (snapshot) {
 
-        console.log(`${snapshot.val().username} left the game :(`);
+        // console.log(`${snapshot.val().username} left the game :(`);
+
+        let disconnectedUser = snapshot.val().username;
+
+
+        database.ref(`/chat/${disconnectedUser}`).set({
+            message: `${disconnectedUser} has disconnected`,
+            sender: 'Chatbot'
+        });
 
     });
 
-   
-// ROCK PAPER SCISSORS GAME LOGIC /////////////////////////////////////////////////////////
+
+    // ROCK PAPER SCISSORS GAME LOGIC ////////////////////////////////////////////////////
     function rpsLogic(p1Choice, p2Choice) {
 
 
@@ -392,7 +528,7 @@ $(document).ready(function () {
             newGame();
 
         } else if (p1Choice === p2Choice) {
-            console.log('tie');
+            // console.log('tie');
             newGame();
         }
 
@@ -411,20 +547,20 @@ $(document).ready(function () {
     }
 
 
-// STARTING A NEW GAME ////////////////////////////////////////////////////////////////////
+    // STARTING A NEW GAME ///////////////////////////////////////////////////////////////
 
     function newGame() {
-        console.log('new game');
+        // console.log('new game');
         database.ref('/turns').set({
             turn: initialTurn
         });
 
     }
 
-// TRACKING ALL CONNECTIONS ////////////////////////////////////////////////////////////// 
+    // TRACKING ALL CONNECTIONS ///////////////////////////////////////////////////////// 
 
     function connectionTracker(p) {
-        console.log(`guest count in tracker ${guestCount}`);
+        // console.log(`guest count in tracker ${guestCount}`);
         let fbPath = "";
 
         switch (p) {
@@ -442,28 +578,138 @@ $(document).ready(function () {
                 break;
         }
 
-        console.log('Path 1', fbPath);
+        // console.log('Path 1', fbPath);
 
         connectedRef.on("value", function (snap) {
 
             // If they are connected..
             if (snap.val()) {
 
-                console.log('Path 2', fbPath);
+                // console.log('Path 2', fbPath);
                 // Add user to the connections list.
                 var con = connectionsRef.push(true);
 
                 // Remove user from the connection list when they disconnect.
 
                 con.onDisconnect().remove();
-                database.ref(fbPath).onDisconnect().remove(() => {
-                    console.log(`${p} disconnected`);
-
-                });
+                database.ref(fbPath).onDisconnect().remove();
 
 
             }
         });
+
+
+    }
+
+
+    function joinGame() {
+
+        let player = myUsername;
+
+        playersRef.once('value').then(function (snapshot) {
+            numPlayers = snapshot.numChildren();
+
+            // console.log('numPlayers 2', numPlayers);
+
+            if (numPlayers === 0) {
+
+                $('#player-name').text(`Hi ${player}! You are Player 1`);
+
+                // $('#player-two-username').append(`<span>Waiting for Player 2</span>`);
+
+
+                database.ref('/players/p1').set({
+                    username: player,
+                    wins: 0,
+                    losses: 0
+                });
+
+                database.ref(`guests/${youAre}`).remove();
+
+                youAre = "playerOne"
+
+
+                playerOneGameRender();
+                newGame();
+            }
+
+            if (numPlayers === 1 && snapshot.child('p1').exists()) {
+
+                $('#player-name').text(`Hi ${player}! You are Player 2`);
+
+                $('#player-turn').text(`Waiting for ${playerOne} to choose`);
+
+
+                database.ref('/players/p2').set({
+                    username: player,
+                    wins: 0,
+                    losses: 0
+                });
+
+                database.ref('/turns').set({
+                    turn: turn
+                });
+
+                database.ref(`guests/${youAre}`).remove();
+
+                youAre = "playerTwo"
+
+
+                playerTwoGameRender();
+                newGame();
+            }
+
+            if (numPlayers === 1 && snapshot.child('p2').exists()) {
+
+                $('#player-name').text(`Hi ${player}! You are Player 1`);
+
+
+                database.ref('/players/p1').set({
+                    username: player,
+                    wins: 0,
+                    losses: 0
+                });
+
+                database.ref('/turns').set({
+                    turn: turn
+                });
+
+                database.ref(`guests/${youAre}`).remove();
+
+                youAre = "playerOne"
+
+
+                playerOneGameRender();
+                newGame();
+
+            }
+
+            connectionTracker(youAre);
+
+        });
+    }
+
+    function playerOneGameRender() {
+
+
+        $('#player-turn').text(`It's your turn!`);
+
+        $('#player-one-rock').html('<p class="rps-selection" id="rock" data-value="rock">Rock</p>');
+        $('#player-one-paper').html('<p class="rps-selection" id="paper" data-value="paper">Paper</p>');
+        $('#player-one-scissors').html('<p class="rps-selection" id="scissors" data-value="scissors">Scissors</p>');
+
+
+    }
+
+    function playerTwoGameRender() {
+
+        $('#player-turn').text(`It's your turn!`);
+
+        $('#player-two-rock').html('<p class="rps-selection" id="rock" data-value="rock">Rock</p>');
+
+        $('#player-two-paper').html('<p class="rps-selection" id="paper" data-value="paper">Paper</p>');
+
+        $('#player-two-scissors').html('<p class="rps-selection" id="scissors" data-value="scissors">Scissors</p>');
 
 
     }
